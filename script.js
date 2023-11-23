ed341dbfd9645d91ff32b561446edbbe3a70c510
Zuhriddin1804 <sotvoldiyevzuhriddin52@gmail.com>
2023-11-24 00:19:06 +0500

    Burger and close buttons have been given an addEventListener function called click

---
+++ script.js
@@ -0,0 +1,16 @@
+// MOBILE NAV FUNCTION
+const mobileNav = () => {
+    const elMobileNav = document.querySelector('#js-mobile-nav');
+    const elBurgerBtn = document.querySelector('#js-burger-btn');
+    const elCloseBtn = document.querySelector('#js-close-btn');
+
+    elBurgerBtn.addEventListener('click', () => {
+        elMobileNav.classList.remove('hidden');
+    });
+
+    elCloseBtn.addEventListener('click', () => {
+        elMobileNav.classList.add('hidden');
+    });
+};
+
+mobileNav();
